8c4f978bf5a9818dc5f3101113bb0889c79351c6
Devanand jha <deva.jha@gmail.com>
2017-07-20 21:30:06 -0500

    logger middleware added

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var express = require('express')
 var app = express()
 
+var logger = function(req,res,next){
+console.log('loading..')
+next();
+};
+
+app.use(logger);
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
