9dd0326b0281fded918fd8449fa6dc6cbf65d711
Devanand jha <deva.jha@gmail.com>
2017-07-27 21:22:56 -0500

    added body parser and nodemon, also use json response

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
+var path = require('path');
 
+/*
 var logger = function(req,res,next){
 console.log('loading..')
 next();
@@ -8,8 +11,30 @@ next();
 
 app.use(logger);
 
+*/
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended: false}));
+
+//app.use(express.static(path.join(__dirname,'public')));
+
+var person =[
+  {
+  name:'Devanand',
+  age:35
+  },
+  {
+    Name:'Utkarsh',
+    Age:7
+  },
+  {
+    name: 'Annu',
+    age:30
+  }
+]
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  //res.send('Hello Node World!')
+  res.json(person);
 })
 
 app.listen(3000, function () {
